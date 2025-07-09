2abd3f0f4eaaf726d9fe64d37ab50ede4c0f00ff
rahul-antares <rahul@antares-tech.com>
2025-07-09 21:34:19 +0530

    made quite a lot of changes

--- init.lua
+++ init.lua
@@ -261,9 +261,13 @@ require("lazy").setup({
             },
         },
     },
-    { "Bilal2453/luvit-meta", lazy = true },
+
     {
-        -- Main LSP Configuration
+        "Bilal2453/luvit-meta",
+        lazy = true
+    },
+
+    {
         "neovim/nvim-lspconfig",
         dependencies = {
             -- Automatically install LSPs and related tools to stdpath for Neovim
@@ -279,83 +283,27 @@ require("lazy").setup({
             "hrsh7th/cmp-nvim-lsp",
         },
         config = function()
-            -- Brief aside: **What is LSP?**
-            --
-            -- LSP is an initialism you've probably heard, but might not understand what it is.
-            --
-            -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-            -- and language tooling communicate in a standardized fashion.
-            --
-            -- In general, you have a "server" which is some tool built to understand a particular
-            -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-            -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-            -- processes that communicate with some "client" - in this case, Neovim!
-            --
-            -- LSP provides Neovim with features like:
-            --  - Go to definition
-            --  - Find references
-            --  - Autocompletion
-            --  - Symbol Search
-            --  - and more!
-            --
-            -- Thus, Language Servers are external tools that must be installed separately from
-            -- Neovim. This is where `mason` and related plugins come into play.
-            --
-            -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-            -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
-            --  This function gets run when an LSP attaches to a particular buffer.
-            --    That is to say, every time a new file is opened that is associated with
-            --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-            --    function will be executed to configure the current buffer
             vim.api.nvim_create_autocmd("LspAttach", {
                 group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
                 callback = function(event)
-                    -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-                    -- to define small helper and utility functions so you don't have to repeat yourself.
-                    --
-                    -- In this case, we create a function that lets us more easily define mappings specific
-                    -- for LSP related items. It sets the mode, buffer and description for us each time.
                     local map = function(keys, func, desc, mode)
                         mode = mode or "n"
                         vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
                     end
 
-                    -- Jump to the definition of the word under your cursor.
-                    --  This is where a variable was first declared, or where a function is defined, etc.
-                    --  To jump back, press <C-t>.
                     map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-
-                    -- Find references for the word under your cursor.
                     map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-
-                    -- Jump to the implementation of the word under your cursor.
-                    --  Useful when your language has ways of declaring types without an actual implementation.
                     map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-
-                    -- Jump to the type of the word under your cursor.
-                    --  Useful when you're not sure what type a variable is and you want to see
-                    --  the definition of its *type*, not where it was *defined*.
                     map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-
-                    -- Fuzzy find all the symbols in your current document.
-                    --  Symbols are things like variables, functions, types, etc.
                     map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
 
-                    -- Fuzzy find all the symbols in your current workspace.
-                    --  Similar to document symbols, except searches over your entire project.
                     map(
                         "<leader>ws",
                         require("telescope.builtin").lsp_dynamic_workspace_symbols,
                         "[W]orkspace [S]ymbols"
                     )
 
-                    -- Rename the variable under your cursor.
-                    --  Most Language Servers support renaming across files, etc.
                     map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-
-                    -- Execute a code action, usually your cursor needs to be on top of an error
-                    -- or a suggestion from your LSP for this to activate.
                     map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
 
                     -- WARN: This is not Goto Definition, this is Goto Declaration.
@@ -742,6 +690,7 @@ require("lazy").setup({
             })
         end,
     },
+
     {
         "akinsho/bufferline.nvim",
         version = "*", -- Specify a version or keep it as "*" for the latest
@@ -818,29 +767,29 @@ require("lazy").setup({
     --         vim.cmd('colorscheme kanagawa')
     --     end,
     -- },
-    {
-        "navarasu/onedark.nvim",
-        lazy = false, -- Load immediately
-        priority = 1000, -- Load before other plugins
-        config = function()
-            require("onedark").setup({
-                style = "dark",  -- Options: 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer'
-                transparent = true,
-                term_colors = false,  -- Use terminal colors
-                code_style = {
-                    comments = "italic",
-                    keywords = "bold",
-                    functions = "bold",
-                    strings = "none",
-                    variables = "none"
-                },
-                lualine = {
-                    transparent = true,  -- Make lualine match the theme
-                }
-            })
-            require("onedark").load()
-        end,
-    }
+    -- {
+    --     "navarasu/onedark.nvim",
+    --     lazy = false, -- Load immediately
+    --     priority = 1000, -- Load before other plugins
+    --     config = function()
+    --         require("onedark").setup({
+    --             style = "dark",  -- Options: 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer'
+    --             transparent = true,
+    --             term_colors = false,  -- Use terminal colors
+    --             code_style = {
+    --                 comments = "italic",
+    --                 keywords = "bold",
+    --                 functions = "bold",
+    --                 strings = "none",
+    --                 variables = "none"
+    --             },
+    --             lualine = {
+    --                 transparent = true,  -- Make lualine match the theme
+    --             }
+    --         })
+    --         require("onedark").load()
+    --     end,
+    -- }
 })
 
 vim.api.nvim_set_hl(0, "Visual", { bg = "#003161", fg = 'white'})-- Set up cursor appearance with specific color and dimensions
@@ -851,10 +800,10 @@ vim.api.nvim_set_hl(0, "Cursor", { bg = "#384BA0", fg="white"}) -- Normal mode c
 vim.api.nvim_set_hl(0, "lCursor", { bg = "#134B70", fg = "white" }) -- Cursor in visual mode
 vim.opt.fillchars:append({ eob = " " })
 
--- vim.cmd [[
---   highlight Normal guibg=none
---   highlight NonText guibg=none
---   highlight Normal ctermbg=none
---   highlight NonText ctermbg=none
--- ]]
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
 --
